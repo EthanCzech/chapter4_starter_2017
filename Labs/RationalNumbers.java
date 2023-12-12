c949b37cf38541df3331fbaa304a4159e5d419f3
EthanCzech <142510740+EthanCzech@users.noreply.github.com>
2023-12-11 18:26:58 -0600

    Update RationalNumbers.java

--- Labs/RationalNumbers.java
+++ Labs/RationalNumbers.java
@@ -16,20 +16,22 @@ public class RationalNumbers
         //use toString to print
       System.out.println ("First rational number: " + r1);
       System.out.println ("Second rational number: " + r2);
-      System.out.println("Subtracted rational numbers:"+ r1.subtract(r2));
-        //are the rational numbers equal?
-      if (true)
+      
+        
+      //are the rational numbers equal?
+      boolean answer = r1.equals(r2);
+      if (answer = true)
          System.out.println ("r1 and r2 are equal.");
       else
          System.out.println ("r1 and r2 are NOT equal.");
 
-      //r3 = //reciprocal of r1
-      //System.out.println ("The reciprocal of r1 is: " + r3);
+      r3 = r1.reciprocal();
+      System.out.println ("The reciprocal of r1 is: " + r3)
 
-      //r4 = //add r1 & r2
-      //r5 = //subtract r1 & r2
-      //r6 = //multiply r1 & r2
-      //r7 = //divide r1 & r2;
+      r4 = r1.add(r2);
+      r5 = r1.subtract(r2);
+      r6 = r1.multiply(r2);
+      r7 = r1.divide(r2);
 
       //System.out.println (r1 + " + " + r2 + " = " + r4);
       //System.out.println (r1 + " - " + r2 + " = " + r5);
