31ea75fa492345fdbf3710adc0dfce97247ae081
EthanCzech <142510740+EthanCzech@users.noreply.github.com>
2023-12-12 18:32:49 -0600

    Update Rational.java

--- Assignments/Rational.java
+++ Assignments/Rational.java
@@ -1,8 +1,7 @@
 /**
-*   Name
-*	Date
-	Class name
-	Description
+*   This is not my actual rational numbers code. The actual version is in labs, as I didn't know there was a copy here to use. B=The Accounts/Banking program is here, sorry for any inconvience
+*	
+	
 ******************************************************************* */
 
 public class Rational
